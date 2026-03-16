e0d4519f8573a821226d82b020594c76086f39af
sasahaa <104555378+sasahaa@users.noreply.github.com>
2026-03-16 19:08:09 +0200

    Update Doctor.java

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class Doctor {
+    private String fullName;
+    private String specialization;
+    private String phoneNumber;
+    private String email;
+
+
+    public Doctor(String fullName, String specialization, String phoneNumber, String email){
+        this.fullName = fullName;
+        this.specialization = specialization;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public void treatPatient(Patient patient) {
+        System.out.println("Doctor " + fullName + " treats " + patient.getName());
+    }
 }
