0331019aa15875f61fd44ddfd5b05559c7ca8c8b
sasahaa <104555378+sasahaa@users.noreply.github.com>
2026-03-16 19:08:34 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Hospital {
+public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        Hospital hospital1 = new Hospital("City Hospital", "Shevchenko St.");
+        hospital1.showHospital();
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+        //inner class
+        Hospital.Administrator admin1 = hospital1.new Administrator("Anna Sasaha", "0988868686", "as@gmail.com");
+        admin1.manageHospital();
+
+        //static nested class
+        Hospital.Department department1 = new Hospital.Department("Surgery", 3);
+        department1.showDepartment();
+
+        //association
+        Doctor doctor1 = new Doctor("A A", "Cardiologist", "0988868686", "aa@gmail.com");
+        Patient patient1 = new Patient("B B", "female", "0988818888", "Shevchenko St.", " bb@gmail.com");
+
+        doctor1.treatPatient(patient1);
+
+        //local class
+        class Reception{
+            void createReception() {
+                System.out.println("Reception created for patient.");
+            }
         }
+
+        Reception reception1 = new Reception();
+        reception1.createReception();
+
+
     }
 }
